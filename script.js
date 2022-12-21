6218975cd3d7bcf4db695b27085a65c7f2e7b6ab
Kenluk234 <114371901+Kenluk234@users.noreply.github.com>
2022-12-21 14:17:03 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+$( ".lesson-icon").first().hover(function(){
+    $( ".text-contents").first().show();
+  }, function(){
+    $(".text-contents").hide();
+  })
+$( ".lesson-icon").eq(1).hover(function(){
+    $( ".text-contents").eq(1).show();
+  }, function(){
+    $(".text-contents").hide();
+  })
+$( ".lesson-icon").eq(2).hover(function(){
+    $( ".text-contents").eq(2).show();
+  }, function(){
+    $(".text-contents").hide();
+  })
+$( ".lesson-icon").eq(3).hover(function(){
+    $( ".text-contents").eq(3).show();
+  }, function(){
+    $(".text-contents").hide();
+  })
